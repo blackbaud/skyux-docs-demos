3eccd0804bd629e3bda71c84715047a6c83aa454
Trevor Burch <Trevor.Burch@blackbaud.com>
2019-09-25 10:41:24 -0400

    URL/Email reactive validators (#18)
    * added reactive form validation for url/email
    
    * Small styling tweaks.
    
    * Updated package-lock
    
    * Fixed build issue
    
    * Remove colons from field labels
    
    * Remove colons from field labels and capitalize URL

--- src/demos/email-validation/email-validation-demo.component.ts
+++ src/demos/email-validation/email-validation-demo.component.ts
@@ -1,11 +1,38 @@
 import {
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup
+} from '@angular/forms';
+
+import {
+  SkyValidators
+} from '@skyux/validation';
 
 @Component({
   selector: 'sky-email-validation-demo',
   templateUrl: './email-validation-demo.component.html'
 })
-export class SkyEmailValidationDemoComponent {
-  public emailValidator: string;
+export class SkyEmailValidationDemoComponent implements OnInit {
+
+  public reactiveForm: FormGroup;
+  public templateDrivenModel: string;
+
+  constructor(
+    private readonly formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.createReactiveForm();
+  }
+
+  private createReactiveForm(): void {
+    this.reactiveForm = this.formBuilder.group({
+      email: new FormControl('', SkyValidators.email)
+    });
+  }
 }
--- src/demos/url-validation/url-validation-demo.component.ts
+++ src/demos/url-validation/url-validation-demo.component.ts
@@ -1,11 +1,38 @@
 import {
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup
+} from '@angular/forms';
+
+import {
+  SkyValidators
+} from '@skyux/validation';
 
 @Component({
   selector: 'sky-url-validation-demo',
   templateUrl: './url-validation-demo.component.html'
 })
-export class SkyUrlValidationDemoComponent {
-  public url: string;
+export class SkyUrlValidationDemoComponent implements OnInit {
+
+  public reactiveForm: FormGroup;
+  public urlValidator: string;
+
+  constructor(
+    private readonly formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.createReactiveForm();
+  }
+
+  private createReactiveForm(): void {
+    this.reactiveForm = this.formBuilder.group({
+      url: new FormControl('', SkyValidators.url)
+    });
+  }
 }
